e1ab2617d5a746cfb15389d68dccbc7e7c591dc5
JuaannY <juanpablobercovsky550@gmail.com>
2024-11-04 13:28:34 -0300

    Added JSONFiles.

---
+++ src/main/java/org/sube/project/util/JSONFiles.java
@@ -0,0 +1,6 @@
+package org.sube.project.util;
+
+public class JSONFiles {
+    public static final String ACCOUNTS_FILE = "accounts.json";
+    public static final String CARDS_FILE = "cards.json";
+}
