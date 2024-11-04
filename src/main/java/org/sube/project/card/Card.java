7ad0b269eff95e5f2258fc5552512646b01358b6
Ian <ian_huth_@hotmail.com>
2024-11-04 13:37:48 -0300

    added cardToJSON in Card.java added userToJSON in User.java

--- src/main/java/org/sube/project/card/Card.java
+++ src/main/java/org/sube/project/card/Card.java
@@ -1,5 +1,7 @@
 package org.sube.project.card;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.sube.project.card.transaction.Transaction;
 import org.sube.project.card.transaction.TransactionType;
 import org.sube.project.util.JSONSube;
@@ -31,6 +33,20 @@ public class Card {
         transactionHistory.add(new Transaction(TransactionType.RECHARGE, amount));
         // CONTINUAR ACA, NO SE COMO REGISTRAR LA TRANSACCION POR LA ID.
         return amount;
+    }
+
+    public JSONObject cardToJSON(){
+        JSONObject j=new JSONObject();
+        try {
+            j.put("id",this.id);
+            j.put("balance",this.balance);
+            j.put("transactionHsitory",this.transactionHistory);
+            j.put("status",this.status);
+        }catch (JSONException jx){
+            System.out.println(jx.getMessage());
+        }
+
+        return j;
     }
 
     public static String getLocalDateTimeNow() {
